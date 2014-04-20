1d519118aeb51f6f1a2c34ce98f619b2b9c51115
Dmitrijs Milajevs <dimazest@gmail.com>
2014-04-20 21:12:06 +0100

    Qsort.py cleanup

--- Qsort.py
+++ Qsort.py
@@ -1,65 +1,52 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
 from sys import argv
 
+
 def quicksort(a, left, right):
-	if (left < right):
-		pivotIdx = left + ((right - left) // 2)
-		newPivotIdx = partition(a, left, right, pivotIdx)
-		quicksort(a, left, newPivotIdx - 1)
-		quicksort(a, newPivotIdx + 1, right)
+    if left < right:
+        pivotIdx = left + ((right - left) // 2)
+        newPivotIdx = partition(a, left, right, pivotIdx)
+        quicksort(a, left, newPivotIdx - 1)
+        quicksort(a, newPivotIdx + 1, right)
+
 
 def partition(a, left, right, pivotIdx):
-	pivotVal = a[pivotIdx]
-	swap(a, pivotIdx, right)
-	storeIdx = left
-	for i in range(left, right):
-		if(a[i] < pivotVal):
-			swap(a, i, storeIdx)
-			storeIdx += 1
-	swap(a, storeIdx, right)
-	return storeIdx
+    pivotVal = a[pivotIdx]
+    a[pivotIdx], a[right] = a[right], a[pivotIdx]
+    storeIdx = left
+    for i in range(left, right):
+        if(a[i] < pivotVal):
+            a[i], a[storeIdx] = a[storeIdx], a[i]
+            storeIdx += 1
 
-def swap(a, x, y):
-	tmp = a[x]
-	a[x] = a[y]
-	a[y] = tmp
+    a[storeIdx], a[right] = a[right], a[storeIdx]
+    return storeIdx
 
-def init_array(a, filename):
-	iF = open(filename, "r")
-	line = iF.readline()
-	i = 0;
-	while (line):
-		a[i] = int(line)
-		line = iF.readline()
-		i+=1
-	iF.close()
 
-#def print_array(a, filename):
-	#oF = open(filename, "w")
-	#for i in range(0, len(a) - 1):
-		#oF.write("{}\n".format(a[i]))
-	#oF.close()
-	
-def pow_ten(idx):
-	 if(idx == 0):
-	      return 1
-	 else:
-	      return 10 * pow_ten(idx - 1)
+def init_array(filename):
+    with open(filename, 'r') as f:
+        for line in f:
+            yield int(line)
+
+
+# def print_array(a, filename):
+#     with open(filename, 'w') as f:
+#         f.write('\n'.join(str(n) for n in a))
+
 
 def main():
-		if(len(argv) != 2):
-			print("Usage ./Qsort.py <powerOfTen>");
-			return
-		IDX = int(argv[1])
-		ARRAY_SIZE = pow_ten(IDX)
-		a = [None] * ARRAY_SIZE
-		ifName = "10e{}.txt".format(IDX)
-		#ofName = "10e{}sortedPY.txt".format(IDX)
-		init_array(a, ifName)
-		quicksort(a, 0, ARRAY_SIZE - 1)
-		print("Sorted ",ARRAY_SIZE," ints in python.")
-		#Used to test sorting
-		#print_array(a, ofName)
+    if len(argv) != 2:
+        print("Usage ./Qsort.py <powerOfTen>")
+        return
+    IDX = int(argv[1])
 
-main()
+    ifName = "10e{}.txt".format(IDX)
+    a = list(init_array(ifName))
+    quicksort(a, 0, len(a) - 1)
+    print("Sorted {} ints in python.".format(len(a)))
+    #Used to test sorting
+    # print_array(a, "10e{}sortedPY.txt".format(IDX))
+
+if __name__ == '__main__':
+    main()
