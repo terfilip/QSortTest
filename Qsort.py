69f9b16ec6fdc7e2c96756b8de8016508f33f7d3
Filip-Ter <filip.ter@gmail.com>
2014-04-21 14:47:24 +0100

    Few changes to python source, as Jython does not support 2.7

--- Qsort.py
+++ Qsort.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
+from __future__ import with_statement
 from sys import argv
+
 
 
 def quicksort(a, left, right):
@@ -41,12 +42,12 @@ def main():
         return
     IDX = int(argv[1])
 
-    ifName = "10e{}.txt".format(IDX)
+    ifName = "10e%d.txt" % IDX
     a = list(init_array(ifName))
     quicksort(a, 0, len(a) - 1)
-    print("Sorted {} ints in python.".format(len(a)))
     #Used to test sorting
-    # print_array(a, "10e{}sortedPY.txt".format(IDX))
+    # print_array(a, "10e%dsortedPY.txt" % IDX)
+    print("Sorted %d ints in python." % len(a))
 
 if __name__ == '__main__':
     main()
